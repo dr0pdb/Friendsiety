7c3a8983f24ea6996799303446b8127c09f13526
srv-twry <saurav007tiwary@gmail.com>
2018-04-06 02:03:29 +0530

    feat: Finalise the accounts

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,5 +19,7 @@ from mysite import views
 
 urlpatterns = [
     url('admin/', admin.site.urls),
-    url(r'^$', views.HomePage.as_view(), name = 'home')
+    url(r'^$', views.HomePage.as_view(), name = 'home'),
+    url(r'accounts/', include('accounts.urls', namespace = 'accounts')),
+    url(r'accounts/', include('django.contrib.auth.urls')),
 ]
